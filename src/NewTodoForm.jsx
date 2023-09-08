5be5cae32f99325c2e28d0c1c883187683af7d2e
Suraj5260 <suraj.telrandhe16@gmail.com>
2023-09-08 20:23:04 +0530

    0

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -23,7 +23,7 @@ export function NewTodoForm({ onSubmit }) {
 					id="item"
 				/>
 			</div>
-			<button id="submit" className="btn">
+			<button id="ADD" className="btn">
 				Add
 			</button>
 		</form>
